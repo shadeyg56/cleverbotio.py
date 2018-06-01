9c7a687d2c0d1dd0d9c5411bdbe7803dd5c32ce0
shadeyg56 <31134255+shadeyg56@users.noreply.github.com>
2018-06-01 14:50:19 -0500

    Delete cleverbottest.py

--- Cleverbotio.py/cleverbottest.py
+++
@@ -1,8 +0,0 @@
-from cleverbot import Cleverbot
-
-cb = Cleverbot("ud577fnPv6KuTKJ8", "XN0Q5y6wpHn83S9NZXuNf3PJnTc75qXD", "testing2")
-
-
-cb.create_session()
-c = cb.say("What is today?")
-print(c)
